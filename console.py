e744caf76c20c69160adfa5646830e42b4338a7d
kyaloeriq <kyaloeriq@gmail.com>
2024-04-20 06:19:54 -0700

    the console file

--- console.py
+++ console.py
@@ -74,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -151,7 +151,8 @@ class HBNBCommand(cmd.Cmd):
                 params[key] = value
 
         # Set updated_at attribute to current datetime
-        params['updated_at'] = datetime.utcnow()
+        params['updated_at'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        params['created_at'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
 
         # Create object with given parameters
         new_instance = HBNBCommand.classes[class_name](**params)
@@ -305,7 +306,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -313,10 +314,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
