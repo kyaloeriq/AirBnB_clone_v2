c6433a2cb65c8f3f79f7b01b644689625cf48007
kyaloeriq <kyaloeriq@gmail.com>
2024-04-20 07:01:44 -0700

    the state class

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import uuid
 
 
 class State(BaseModel):
@@ -8,6 +9,6 @@ class State(BaseModel):
     def __init__(self, *args, **kwargs):
         """Initializes State instance"""
         super().__init__(*args, **kwargs)
-         # Ensure 'id' attribute is set properly
-         if 'id' not in kwargs:
-             self.id = str(uuid.uuid4())
+        # Ensure 'id' attribute is set properly
+        if 'id' not in kwargs:
+            self.id = str(uuid.uuid4())
